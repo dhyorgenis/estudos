263cc8394f9b4d73839359dfd7192a94f20a8490
dhyorgenis <50756927+dhyorgenis@users.noreply.github.com>
2020-02-29 13:45:47 -0300

    Add files via upload
    condicional

---
+++ condicional.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int a =5, opcao=1;
+	float b=2.5;
+	char c='x';
+	
+	
+	if(a==5){
+		
+		printf("a variavel A eh 5\n");
+	}
+	
+	if(b==2.5){
+		
+		printf("a variavel B eh 2.5\n");
+	}
+		
+	
+	if (a %2==1){
+	
+	printf("a variavel A eh impar\n");
+}
+	else {
+		printf("a variavel A eh par\n");
+		}
+		
+		if(c=='x'){
+		
+		printf("a variavel C eh letra X\n");
+	}
+	
+	
+	if(opcao==1){
+		printf("\n a opcao eh igual a 1");
+	}
+		else if(opcao==2){
+			printf("a opcao eh 2");
+		}
+		else{
+			printf("a opcao nao eh igual a 1 nem 2\n");
+		}
+	}
