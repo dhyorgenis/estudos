ec0c094dfc784402f4424faf35b3ca074dc9a096
dhyorgenis <50756927+dhyorgenis@users.noreply.github.com>
2020-02-29 13:04:19 -0300

    Add files via upload
    condicionais

---
+++ condicional.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int a =5;
+	
+	if(a==5){
+		
+		printf("a var A eh 5\n");
+	}
+	
+	if (a %2==1){
+	
+	printf("a variavel eh impar");
+}
+	else {
+		printf("a variavel eh par");
+		}
+	}
+	
